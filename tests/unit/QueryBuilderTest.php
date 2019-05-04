b3fb7206ed431ba493979e235fdf7a45773e86ac
Bizley <pawel@positive.codes>
2019-05-04 18:16:53 +0200

    Tests for alterColumn

--- tests/unit/QueryBuilderTest.php
+++ tests/unit/QueryBuilderTest.php
@@ -159,21 +159,45 @@ class QueryBuilderTest extends \yii\db\tests\unit\QueryBuilderTest
     public function testAlterColumn()
     {
         $qb = $this->getQueryBuilder();
-
-        $expected = 'ALTER TABLE "foo1" ALTER COLUMN "bar" TYPE varchar(255)';
+        
+        $expected = 'ALTER TABLE "foo1" ALTER COLUMN "bar" TYPE varchar(255), ALTER COLUMN "bar" DROP DEFAULT, ALTER COLUMN "bar" DROP NOT NULL';
         $sql = $qb->alterColumn('foo1', 'bar', 'varchar(255)');
         $this->assertEquals($expected, $sql);
-
+        
         $expected = 'ALTER TABLE "foo1" ALTER COLUMN "bar" SET NOT null';
         $sql = $qb->alterColumn('foo1', 'bar', 'SET NOT null');
         $this->assertEquals($expected, $sql);
-
+        
         $expected = 'ALTER TABLE "foo1" ALTER COLUMN "bar" drop default';
         $sql = $qb->alterColumn('foo1', 'bar', 'drop default');
         $this->assertEquals($expected, $sql);
-
+        
         $expected = 'ALTER TABLE "foo1" ALTER COLUMN "bar" reset xyz';
         $sql = $qb->alterColumn('foo1', 'bar', 'reset xyz');
+        $this->assertEquals($expected, $sql);
+        
+        $expected = 'ALTER TABLE "foo1" ALTER COLUMN "bar" TYPE varchar(255), ALTER COLUMN "bar" DROP DEFAULT, ALTER COLUMN "bar" DROP NOT NULL';
+        $sql = $qb->alterColumn('foo1', 'bar', $this->string(255));
+        $this->assertEquals($expected, $sql);
+        
+        $expected = 'ALTER TABLE "foo1" ALTER COLUMN "bar" TYPE varchar(255), ALTER COLUMN "bar" DROP DEFAULT, ALTER COLUMN "bar" SET NOT NULL';
+        $sql = $qb->alterColumn('foo1', 'bar', $this->string(255)->notNull());
+        $this->assertEquals($expected, $sql);
+        
+        $expected = 'ALTER TABLE "foo1" ALTER COLUMN "bar" TYPE varchar(255), ALTER COLUMN "bar" DROP DEFAULT, ALTER COLUMN "bar" DROP NOT NULL, ADD CONSTRAINT foo1_bar_check CHECK (char_length(bar) > 5)';
+        $sql = $qb->alterColumn('foo1', 'bar', $this->string(255)->check('char_length(bar) > 5'));
+        $this->assertEquals($expected, $sql);
+        
+        $expected = 'ALTER TABLE "foo1" ALTER COLUMN "bar" TYPE varchar(255), ALTER COLUMN "bar" SET DEFAULT \'AbCdE\', ALTER COLUMN "bar" DROP NOT NULL';
+        $sql = $qb->alterColumn('foo1', 'bar', $this->string(255)->defaultValue('AbCdE'));
+        $this->assertEquals($expected, $sql);
+        
+        $expected = 'ALTER TABLE "foo1" ALTER COLUMN "bar" TYPE timestamp(0), ALTER COLUMN "bar" SET DEFAULT CURRENT_TIMESTAMP, ALTER COLUMN "bar" DROP NOT NULL';
+        $sql = $qb->alterColumn('foo1', 'bar', $this->timestamp()->defaultExpression('CURRENT_TIMESTAMP'));
+        $this->assertEquals($expected, $sql);
+        
+        $expected = 'ALTER TABLE "foo1" ALTER COLUMN "bar" TYPE varchar(30), ALTER COLUMN "bar" DROP DEFAULT, ALTER COLUMN "bar" DROP NOT NULL, ADD UNIQUE ("bar")';
+        $sql = $qb->alterColumn('foo1', 'bar', $this->string(30)->unique());
         $this->assertEquals($expected, $sql);
     }
 
