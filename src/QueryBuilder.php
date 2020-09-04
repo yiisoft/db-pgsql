c1d112169df5afa98232fbd6c76928be890e8e50
terabytesoftw <terabytesoftw@gmail.com>
2020-09-03 20:52:11 -0400

    Add compatibility php 8.0.

--- src/QueryBuilder.php
+++ src/QueryBuilder.php
@@ -752,7 +752,7 @@ final class QueryBuilder extends AbstractQueryBuilder
                     $value = $schema->quoteValue($value);
                 } elseif (is_float($value)) {
                     /* ensure type cast always has . as decimal separator in all locales */
-                    $value = NumericHelper::normalize($value);
+                    $value = NumericHelper::normalize((string) $value);
                 } elseif ($value === true) {
                     $value = 'TRUE';
                 } elseif ($value === false) {
