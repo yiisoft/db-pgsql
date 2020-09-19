9d42e6ac0c15093aaf87c7fb0d8a5fd6199381dd
Wilmer Arambula <terabytesoftw@gmail.com>
2020-09-19 12:38:54 -0300

    Add SqlProviderTest.

---
+++ tests/SqlDataProviderTest.php
@@ -0,0 +1,15 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Yiisoft\Db\Pgsql\Tests;
+
+use Yiisoft\Db\TestUtility\TestSqlDataProviderTrait;
+
+/**
+ * @group pgsql
+ */
+final class SqlDataProviderTest extends TestCase
+{
+    use TestSqlDataProviderTrait;
+}
