dee259533ba897747423663d15086e12b08c2ddd
Wilmer Arambula <terabytesoftw@gmail.com>
2021-01-25 16:28:14 -0300

    Add more tests.

--- tests/ColumnSchemaTest.php
+++ tests/ColumnSchemaTest.php
@@ -13,24 +13,25 @@ use Yiisoft\Db\Query\Query;
  */
 final class ColumnSchemaTest extends TestCase
 {
-    public function testDbTypes(): void
+    public function testPhpTypeCast(): void
     {
         $db = $this->getConnection(true);
-
         $command = $db->createCommand();
+        $schema = $db->getSchema();
+        $tableSchema = $schema->getTableSchema('type');
 
         $command->insert(
             'type',
             [
                 'int_col' => 1,
                 'char_col' => str_repeat('x', 100),
-                'char_col3' => str_repeat('x', 101),
+                'char_col3' => null,
                 'float_col' => 1.234,
                 'blob_col' => "\x10\x11\x12",
                 'bool_col' => false,
                 'bigint_col' => 9223372036854775806,
                 'intarray_col' => [1, -2, null, '42'],
-                'textarray2_col' => new ArrayExpression([['text'], [null], [1]], 'text', 2),
+                'textarray2_col' => new ArrayExpression(null),
                 'json_col' => [['a' => 1, 'b' => null, 'c' => [1, 3, 5]]],
                 'jsonb_col' => new JsonExpression(new ArrayExpression([1, 2, 3])),
                 'jsonarray_col' => [new ArrayExpression([[',', 'null', true, 'false', 'f']], 'json')],
@@ -41,26 +42,28 @@ final class ColumnSchemaTest extends TestCase
 
         $query = (new Query($db))->from('type')->one();
 
-        $this->assertSame(1, $query['int_col']);
+        $intColPhpTypeCast = $tableSchema->getColumn('int_col')->phpTypecast($query['int_col']);
+        $charColPhpTypeCast = $tableSchema->getColumn('char_col')->phpTypecast($query['char_col']);
+        $floatColPhpTypeCast = $tableSchema->getColumn('float_col')->phpTypecast($query['float_col']);
+        $numericColPhpTypeCast = $tableSchema->getColumn('numeric_col')->phpTypecast($query['numeric_col']);
+        $intArrayColPhpType = $tableSchema->getColumn('intarray_col')->phpTypecast($query['intarray_col']);
+        $textArray2ColPhpType = $tableSchema->getColumn('textarray2_col')->phpTypecast($query['textarray2_col']);
+        $jsonColPhpType = $tableSchema->getColumn('json_col')->phpTypecast($query['json_col']);
+        $jsonBColPhpType = $tableSchema->getColumn('jsonb_col')->phpTypecast($query['jsonb_col']);
+        $jsonArrayColPhpType = $tableSchema->getColumn('jsonarray_col')->phpTypecast($query['jsonarray_col']);
+
+        $this->assertSame(1, $intColPhpTypeCast);
         $this->assertSame(1, $query['int_col2']);
         $this->assertSame(1, $query['tinyint_col']);
         $this->assertSame(1, $query['smallint_col']);
-        $this->assertSame(str_repeat('x', 100), $query['char_col']);
-        $this->assertSame('something', $query['char_col2']);
-        $this->assertSame(str_repeat('x', 101), $query['char_col3']);
-        $this->assertSame('1.234', $query['float_col']);
+        $this->assertSame(str_repeat('x', 100), $charColPhpTypeCast);
+        $this->assertSame(1.234, $floatColPhpTypeCast);
         $this->assertSame('1.23', $query['float_col2']);
-        $this->assertSame("\x10\x11\x12", stream_get_contents($query['blob_col']));
-        $this->assertSame('33.22', $query['numeric_col']);
-        $this->assertSame('2002-01-01 00:00:00', $query['time']);
-        $this->assertSame(false, $query['bool_col']);
-        $this->assertSame(true, $query['bool_col2']);
-        $this->assertSame('10000010', $query['bit_col']);
-        $this->assertSame(9223372036854775806, $query['bigint_col']);
-        $this->assertSame('{1,-2,NULL,42}', $query['intarray_col']);
-        $this->assertSame('{{text},{NULL},{1}}', $query['textarray2_col']);
-        $this->assertSame('[{"a":1,"b":null,"c":[1,3,5]}]', $query['json_col']);
-        $this->assertSame('["1", "2", "3"]', $query['jsonb_col']);
-        $this->assertSame('{{"[\",\",\"null\",true,\"false\",\"f\"]"}}', $query['jsonarray_col']);
+        $this->assertSame('33.22', $numericColPhpTypeCast);
+        $this->assertSame([1, -2, null, 42], $intArrayColPhpType);
+        $this->assertSame(null, $textArray2ColPhpType);
+        $this->assertSame([['a' => 1, 'b' => null, 'c' => [1, 3, 5]]], $jsonColPhpType);
+        $this->assertSame(["1", "2", "3"], $jsonBColPhpType);
+        $this->assertSame([[[',', 'null', true, 'false', 'f']]], $jsonArrayColPhpType);
     }
 }
