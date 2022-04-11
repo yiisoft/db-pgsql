4e189caf7ea8190a6d6b36b8836cbb076138e579
Valerii Gorbachev <darkdef@gmail.com>
2022-04-11 20:01:48 +0300

    Simplification of Connection - removing pool from Connection (#110)
    * Simplification of Connection - removing pool from Connection
    
    * rename method to activePdo and constructor fixes

--- src/PDO/ConnectionPDOPgsql.php
+++ src/PDO/ConnectionPDOPgsql.php
@@ -5,13 +5,10 @@ declare(strict_types=1);
 namespace Yiisoft\Db\Pgsql\PDO;
 
 use PDO;
-use PDOException;
-use Psr\Log\LogLevel;
 use Yiisoft\Db\Cache\QueryCache;
 use Yiisoft\Db\Cache\SchemaCache;
 use Yiisoft\Db\Command\CommandInterface;
-use Yiisoft\Db\Connection\Connection;
-use Yiisoft\Db\Connection\ConnectionPDOInterface;
+use Yiisoft\Db\Connection\ConnectionPDO;
 use Yiisoft\Db\Driver\PDODriver;
 use Yiisoft\Db\Exception\Exception;
 use Yiisoft\Db\Exception\InvalidConfigException;
@@ -22,57 +19,17 @@ use Yiisoft\Db\Schema\SchemaInterface;
 use Yiisoft\Db\Transaction\TransactionInterface;
 
 /**
+ * Database connection class prefilled for PgSQL Server.
  * The class Connection represents a connection to a database via [PDO](https://secure.php.net/manual/en/book.pdo.php).
  */
-final class ConnectionPDOPgsql extends Connection implements ConnectionPDOInterface
+final class ConnectionPDOPgsql extends ConnectionPDO
 {
-    private ?PDO $pdo = null;
-    private ?QueryBuilderInterface $queryBuilder = null;
-    private ?QuoterInterface $quoter = null;
-    private ?SchemaInterface $schema = null;
-    private string $serverVersion = '';
-
     public function __construct(
-        private PDODriver $driver,
-        private QueryCache $queryCache,
-        private SchemaCache $schemaCache
+        protected PDODriver $driver,
+        protected QueryCache $queryCache,
+        protected SchemaCache $schemaCache
     ) {
         parent::__construct($queryCache);
-    }
-
-    /**
-     * Reset the connection after cloning.
-     */
-    public function __clone()
-    {
-        $this->master = null;
-        $this->slave = null;
-        $this->transaction = null;
-
-        if (strncmp($this->driver->getDsn(), 'sqlite::memory:', 15) !== 0) {
-            /** reset PDO connection, unless its sqlite in-memory, which can only have one connection */
-            $this->pdo = null;
-        }
-    }
-
-    /**
-     * Close the connection before serializing.
-     *
-     * @return array
-     */
-    public function __sleep(): array
-    {
-        $fields = (array) $this;
-
-        unset(
-            $fields["\000" . __CLASS__ . "\000" . 'pdo'],
-            $fields["\000" . __CLASS__ . "\000" . 'master'],
-            $fields["\000" . __CLASS__ . "\000" . 'slave'],
-            $fields["\000" . __CLASS__ . "\000" . 'transaction'],
-            $fields["\000" . __CLASS__ . "\000" . 'schema']
-        );
-
-        return array_keys($fields);
     }
 
     public function createCommand(?string $sql = null, array $params = []): CommandInterface
@@ -99,55 +56,9 @@ final class ConnectionPDOPgsql extends Connection implements ConnectionPDOInterf
         return new TransactionPDOPgsql($this);
     }
 
-    public function close(): void
-    {
-        if (!empty($this->master)) {
-            /** @var ConnectionPDOPgsql */
-            $db = $this->master;
-
-            if ($this->pdo === $db->getPDO()) {
-                $this->pdo = null;
-            }
-
-            $db->close();
-            $this->master = null;
-        }
-
-        if ($this->pdo !== null) {
-            $this->logger?->log(
-                LogLevel::DEBUG,
-                'Closing DB connection: ' . $this->driver->getDsn() . ' ' . __METHOD__,
-            );
-
-            $this->pdo = null;
-            $this->transaction = null;
-        }
-
-        if (!empty($this->slave)) {
-            $this->slave->close();
-            $this->slave = null;
-        }
-    }
-
-    public function getDriver(): PDODriver
-    {
-        return $this->driver;
-    }
-
     public function getDriverName(): string
     {
         return 'pgsql';
-    }
-
-    public function getMasterPdo(): PDO|null
-    {
-        $this->open();
-        return $this->pdo;
-    }
-
-    public function getPDO(): ?PDO
-    {
-        return $this->pdo;
     }
 
     /**
@@ -175,20 +86,6 @@ final class ConnectionPDOPgsql extends Connection implements ConnectionPDOInterf
         return $this->quoter;
     }
 
-    /**
-     * @throws Exception
-     */
-    public function getServerVersion(): string
-    {
-        if ($this->serverVersion === '') {
-            /** @var mixed */
-            $version = $this->getSlavePDO()?->getAttribute(PDO::ATTR_SERVER_VERSION);
-            $this->serverVersion = is_string($version) ? $version : 'Version could not be determined.';
-        }
-
-        return $this->serverVersion;
-    }
-
     public function getSchema(): SchemaInterface
     {
         if ($this->schema === null) {
@@ -196,60 +93,6 @@ final class ConnectionPDOPgsql extends Connection implements ConnectionPDOInterf
         }
 
         return $this->schema;
-    }
-
-    public function getSlavePdo(bool $fallbackToMaster = true): ?PDO
-    {
-        /** @var ConnectionPDOPgsql|null $db */
-        $db = $this->getSlave(false);
-
-        if ($db === null) {
-            return $fallbackToMaster ? $this->getMasterPdo() : null;
-        }
-
-        return $db->getPDO();
-    }
-
-    public function isActive(): bool
-    {
-        return $this->pdo !== null;
-    }
-
-    public function open(): void
-    {
-        if (!empty($this->pdo)) {
-            return;
-        }
-
-        if (!empty($this->masters)) {
-            /** @var ConnectionPDOPgsql|null */
-            $db = $this->getMaster();
-
-            if ($db !== null) {
-                $this->pdo = $db->getPDO();
-                return;
-            }
-
-            throw new InvalidConfigException('None of the master DB servers is available.');
-        }
-
-        if (empty($this->driver->getDsn())) {
-            throw new InvalidConfigException('Connection::dsn cannot be empty.');
-        }
-
-        $token = 'Opening DB connection: ' . $this->driver->getDsn();
-
-        try {
-            $this->logger?->log(LogLevel::INFO, $token);
-            $this->profiler?->begin($token, [__METHOD__]);
-            $this->initConnection();
-            $this->profiler?->end($token, [__METHOD__]);
-        } catch (PDOException $e) {
-            $this->profiler?->end($token, [__METHOD__]);
-            $this->logger?->log(LogLevel::ERROR, $token);
-
-            throw new Exception($e->getMessage(), (array) $e->errorInfo, $e);
-        }
     }
 
     /**
