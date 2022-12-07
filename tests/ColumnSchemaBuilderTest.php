b9f91cbd8b9312375df7af0cb541106cf3f23656
Wilmer Arambula <terabytesoftw@gmail.com>
2022-12-07 13:28:26 -0300

    Remove `src\TestSupport\TestColumnSchemaBuilderTrait` from `yiisoft\db`.

--- tests/ColumnSchemaBuilderTest.php
+++ tests/ColumnSchemaBuilderTest.php
@@ -4,20 +4,15 @@ declare(strict_types=1);
 
 namespace Yiisoft\Db\Pgsql\Tests;
 
-use Yiisoft\Db\TestSupport\TestColumnSchemaBuilderTrait;
+use Yiisoft\Db\Pgsql\Tests\Support\TestTrait;
+use Yiisoft\Db\Tests\Common\CommonColumnSchemaBuilderTest;
 
 /**
  * @group pgsql
+ *
+ * @psalm-suppress PropertyNotSetInConstructor
  */
-final class ColumnSchemaBuilderTest extends TestCase
+final class ColumnSchemaBuilderTest extends CommonColumnSchemaBuilderTest
 {
-    use TestColumnSchemaBuilderTrait;
-
-    /**
-     * @dataProvider typesProviderTrait
-     */
-    public function testCustomTypes(string $expected, string $type, ?int $length, mixed $calls): void
-    {
-        $this->checkBuildString($expected, $type, $length, $calls);
-    }
+    use TestTrait;
 }
