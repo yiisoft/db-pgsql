62f80780d169285004efee538f66db28dda6b0c6
Valerii Gorbachev <darkdef@gmail.com>
2023-01-19 22:36:38 +0300

    Yii2 fix 2 (#219)
    * Fixes from yii2 #18435, #18442
    
    * Fixes from yii2 #18435, #18442 and fix defaultValue for mssql

--- src/Schema.php
+++ src/Schema.php
@@ -643,7 +643,7 @@ final class Schema extends AbstractSchema
 
             $column = $row['columnname'];
 
-            if (!empty($column) && $column[0] === '"') {
+            if (str_starts_with($column, '"') && str_ends_with($column, '"')) {
                 /**
                  * postgres will quote names that are not lowercase-only.
                  *
