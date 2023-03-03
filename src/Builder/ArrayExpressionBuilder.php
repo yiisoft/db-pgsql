c45595d3e93abf34246ce3bd231f447f9c5c4b95
Wilmer Arambula <terabytesoftw@gmail.com>
2023-03-03 12:44:08 -0300

    Remove redundancy.

--- src/Builder/ArrayExpressionBuilder.php
+++ src/Builder/ArrayExpressionBuilder.php
@@ -23,7 +23,7 @@ use function is_array;
 use function str_repeat;
 
 /**
- * ArrayExpressionBuilder builds {@see `Yiisoft\Db\Expression\ArrayExpression`} for PostgresSQL Server.
+ * Builds {@see `Yiisoft\Db\Expression\ArrayExpression`} for PostgresSQL Server.
  */
 final class ArrayExpressionBuilder implements ExpressionBuilderInterface
 {
