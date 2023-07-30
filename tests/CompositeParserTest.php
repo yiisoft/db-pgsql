d5f295efa537af28354f4b2911c389eacf41f9f8
Tigrov <rrr-r@ya.ru>
2023-07-30 13:56:51 +0700

    Fix typo in `$compositeParser`

--- tests/CompositeParserTest.php
+++ tests/CompositeParserTest.php
@@ -14,19 +14,19 @@ final class CompositeParserTest extends TestCase
 {
     public function testParser(): void
     {
-        $compositeParse = new CompositeParser();
+        $compositeParser = new CompositeParser();
 
-        $this->assertSame([null], $compositeParse->parse('()'));
-        $this->assertSame([0 => null, 1 => null], $compositeParse->parse('(,)'));
-        $this->assertSame([0 => '10.0', 1 => 'USD'], $compositeParse->parse('(10.0,USD)'));
-        $this->assertSame([0 => '1', 1 => '-2', 2 => null, 3 => '42'], $compositeParse->parse('(1,-2,,42)'));
-        $this->assertSame([0 => ''], $compositeParse->parse('("")'));
+        $this->assertSame([null], $compositeParser->parse('()'));
+        $this->assertSame([0 => null, 1 => null], $compositeParser->parse('(,)'));
+        $this->assertSame([0 => '10.0', 1 => 'USD'], $compositeParser->parse('(10.0,USD)'));
+        $this->assertSame([0 => '1', 1 => '-2', 2 => null, 3 => '42'], $compositeParser->parse('(1,-2,,42)'));
+        $this->assertSame([0 => ''], $compositeParser->parse('("")'));
         $this->assertSame(
             [0 => '[",","null",true,"false","f"]'],
-            $compositeParse->parse('("[\",\",\"null\",true,\"false\",\"f\"]")')
+            $compositeParser->parse('("[\",\",\"null\",true,\"false\",\"f\"]")')
         );
 
         // Default values can have any expressions
-        $this->assertSame(null, $compositeParse->parse("'(10.0,USD)::composite_type'"));
+        $this->assertSame(null, $compositeParser->parse("'(10.0,USD)::composite_type'"));
     }
 }
