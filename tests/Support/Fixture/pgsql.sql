5c998c6e8d6433783539b8067dacb0eff113bd88
Tigrov <rrr-r@ya.ru>
2024-01-04 06:35:15 +0700

    Update fixtures according to tests

--- tests/Support/Fixture/pgsql.sql
+++ tests/Support/Fixture/pgsql.sql
@@ -138,19 +138,19 @@ CREATE TABLE "null_values" (
 
 CREATE TABLE "type" (
   int_col integer NOT NULL,
+  float_col double precision NOT NULL,
+  char_col char(100) NOT NULL,
+  bool_col boolean NOT NULL,
   int_col2 integer DEFAULT '1',
   tinyint_col smallint DEFAULT '1',
   smallint_col smallint DEFAULT '1',
-  char_col char(100) NOT NULL,
   char_col2 varchar(100) DEFAULT 'some''thing',
   char_col3 text,
   char_col4 character varying DEFAULT E'first line\nsecond line',
-  float_col double precision NOT NULL,
   float_col2 double precision DEFAULT '1.23',
   blob_col bytea DEFAULT 'a binary value',
   numeric_col decimal(5,2) DEFAULT '33.22',
   time timestamp NOT NULL DEFAULT '2002-01-01 00:00:00',
-  bool_col boolean NOT NULL,
   bool_col2 boolean DEFAULT TRUE,
   ts_default TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
   bit_col BIT(8) NOT NULL DEFAULT B'10000010', -- 130
