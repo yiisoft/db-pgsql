5cebdcd45fd5a59d468aed33a6215aa4b688c096
Sergei Tigrov <rrr-r@ya.ru>
2024-04-13 15:02:52 +0700

    Allow scalar values for `Query::select()` (#340)
    * Update tests according main PR
    
    * Add type string to test
    
    * Apply Rector changes (CI)
    
    * Revert "Apply Rector changes (CI)"
    
    This reverts commit 230d3fddbd627b7ee304ec8df7bc24650411deb5.
    
    * Disable Rector for tests
    
    ---------
    
    Co-authored-by: Tigrov <Tigrov@users.noreply.github.com>
    Co-authored-by: Sergei Predvoditelev <sergei@predvoditelev.ru>

--- tests/QueryBuilderTest.php
+++ tests/QueryBuilderTest.php
@@ -675,4 +675,10 @@ final class QueryBuilderTest extends CommonQueryBuilderTest
     ): void {
         parent::testUpsertExecute($table, $insertColumns, $updateColumns);
     }
+
+    /** @dataProvider \Yiisoft\Db\Pgsql\Tests\Provider\QueryBuilderProvider::selectScalar */
+    public function testSelectScalar(array|bool|float|int|string $columns, string $expected): void
+    {
+        parent::testSelectScalar($columns, $expected);
+    }
 }
