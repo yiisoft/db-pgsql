84e2b4ae6ed7e099c6c5cbdf81441f631064484d
Tigrov <rrr-r@ya.ru>
2024-10-07 17:19:19 +0700

    Remove `ArrayColumnSchema::getColumnFactory()`

--- src/Column/ArrayColumnSchema.php
+++ src/Column/ArrayColumnSchema.php
@@ -6,16 +6,10 @@ namespace Yiisoft\Db\Pgsql\Column;
 
 use Yiisoft\Db\Pgsql\ArrayParser;
 use Yiisoft\Db\Schema\Column\ArrayColumnSchema as BaseArrayColumnSchema;
-use Yiisoft\Db\Schema\Column\ColumnFactoryInterface;
 use Yiisoft\Db\Syntax\ParserToArrayInterface;
 
 final class ArrayColumnSchema extends BaseArrayColumnSchema
 {
-    protected function getColumnFactory(): ColumnFactoryInterface
-    {
-        return new ColumnFactory();
-    }
-
     protected function getParser(): ParserToArrayInterface
     {
         return new ArrayParser();
