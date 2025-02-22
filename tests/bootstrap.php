3178c40062fc7f0cb45041011342e87346ccd492
Tigrov <rrr-r@ya.ru>
2025-02-22 15:22:30 +0700

    Revert env

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -2,9 +2,7 @@
 
 declare(strict_types=1);
 
-if (getenv('ENVIRONMENT', true) === 'local') {
-    putenv('YII_PGSQL_DATABASE=yii');
-    putenv('YII_PGSQL_HOST=postgres');
-    putenv('YII_PGSQL_USER=postgres');
-    putenv('YII_PGSQL_PASSWORD=postgres');
+if (getenv('ENVIRONMENT', local_only: true) === 'local') {
+    $dotenv = Dotenv\Dotenv::createUnsafeImmutable(__DIR__);
+    $dotenv->load();
 }
