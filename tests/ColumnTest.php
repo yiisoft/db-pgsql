f8178f09c304f9b1ed5f0d2a2225180c1edbc63e
Tigrov <rrr-r@ya.ru>
2025-03-27 21:57:39 +0700

    Improve test coverage

--- tests/ColumnTest.php
+++ tests/ColumnTest.php
@@ -113,8 +113,7 @@ final class ColumnTest extends AbstractColumnTest
     {
         $db = $this->getConnection(true);
 
-        $result = $db->createCommand(
-            <<<SQL
+        $sql = <<<SQL
             SELECT
                 null AS "null",
                 1 AS "1",
@@ -127,25 +126,37 @@ final class ColumnTest extends AbstractColumnTest
                 '{1,2,3}'::int[] AS "intarray",
                 '{"a":1}'::jsonb AS "jsonb",
                 '(10,USD)'::currency_money_structured AS "composite"
-            SQL
-        )->phpTypecasting()->queryOne();
+            SQL;
 
-        $this->assertSame(
-            [
-                'null' => null,
-                1 => 1,
-                '2.5' => 2.5,
-                'true' => true,
-                'false' => false,
-                'string' => 'string',
-                'enum' => 'VAL1',
-                'enum2' => 'VAL2',
-                'intarray' => [1, 2, 3],
-                'jsonb' => ['a' => 1],
-                'composite' => ['value' => 10.0, 'currency_code' => 'USD'],
-            ],
-            $result,
-        );
+        $expected = [
+            'null' => null,
+            1 => 1,
+            '2.5' => 2.5,
+            'true' => true,
+            'false' => false,
+            'string' => 'string',
+            'enum' => 'VAL1',
+            'enum2' => 'VAL2',
+            'intarray' => [1, 2, 3],
+            'jsonb' => ['a' => 1],
+            'composite' => ['value' => 10.0, 'currency_code' => 'USD'],
+        ];
+
+        $result = $db->createCommand($sql)->phpTypecasting()->queryOne();
+
+        $this->assertSame($expected, $result);
+
+        $result = $db->createCommand($sql)->phpTypecasting()->queryAll();
+
+        $this->assertSame([$expected], $result);
+
+        $result = $db->createCommand('SELECT 2.5')->phpTypecasting()->queryScalar();
+
+        $this->assertSame(2.5, $result);
+
+        $result = $db->createCommand('SELECT 2.5 UNION SELECT 3.3')->phpTypecasting()->queryColumn();
+
+        $this->assertSame([2.5, 3.3], $result);
 
         $db->close();
     }
