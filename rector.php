9b218d82b2a42ed6bf621eb9de883e68ea97565d
Sergei Tigrov <rrr-r@ya.ru>
2025-06-27 15:15:27 +0700

    Refactor workflows (#419)

--- rector.php
+++ rector.php
@@ -6,27 +6,17 @@ use Rector\CodeQuality\Rector\Class_\InlineConstructorDefaultToPropertyRector;
 use Rector\Config\RectorConfig;
 use Rector\Php81\Rector\FuncCall\NullToStrictStringFuncCallArgRector;
 use Rector\Php81\Rector\Property\ReadOnlyPropertyRector;
-use Rector\Set\ValueObject\LevelSetList;
 
-return static function (RectorConfig $rectorConfig): void {
-    $rectorConfig->paths([
+return RectorConfig::configure()
+    ->withPaths([
         __DIR__ . '/src',
-        /**
-         * Disabled ./tests directory due to different branches with main package when testing
-         */
-        // __DIR__ . '/tests',
-    ]);
-
-    // register a single rule
-    $rectorConfig->rule(InlineConstructorDefaultToPropertyRector::class);
-
-    // define sets of rules
-    $rectorConfig->sets([
-        LevelSetList::UP_TO_PHP_81,
-    ]);
-
-    $rectorConfig->skip([
+        __DIR__ . '/tests',
+    ])
+    ->withPhpSets(php81: true)
+    ->withRules([
+        InlineConstructorDefaultToPropertyRector::class,
+    ])
+    ->withSkip([
         NullToStrictStringFuncCallArgRector::class,
         ReadOnlyPropertyRector::class,
     ]);
-};
