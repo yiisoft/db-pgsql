aa1f530a68bd87323e7f9f891356ae11d151ec22
Sergei Predvoditelev <sergei@predvoditelev.ru>
2025-08-09 22:01:46 +0300

    adapt

--- src/Builder/LikeBuilder.php
+++ src/Builder/LikeBuilder.php
@@ -4,7 +4,6 @@ declare(strict_types=1);
 
 namespace Yiisoft\Db\Pgsql\Builder;
 
-use Yiisoft\Db\QueryBuilder\Condition\AbstractLike;
 use Yiisoft\Db\QueryBuilder\Condition\Like;
 use Yiisoft\Db\QueryBuilder\Condition\NotLike;
 
@@ -13,12 +12,11 @@ use Yiisoft\Db\QueryBuilder\Condition\NotLike;
  */
 final class LikeBuilder extends \Yiisoft\Db\QueryBuilder\Condition\Builder\LikeBuilder
 {
-    protected function getOperatorData(AbstractLike $condition): array
+    protected function getOperatorData(Like|NotLike $condition): array
     {
         return match ($condition::class) {
             Like::class => [false, $condition->caseSensitive === false ? 'ILIKE' : 'LIKE'],
             NotLike::class => [true, $condition->caseSensitive === false ? 'NOT ILIKE' : 'NOT LIKE'],
-            default => $this->throwUnsupportedConditionException($condition),
         };
     }
 }
