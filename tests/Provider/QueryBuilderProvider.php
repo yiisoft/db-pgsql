3ada594dbe0f43abf398aec84e13ebd634ab8355
Tigrov <8563175+Tigrov@users.noreply.github.com>
2025-08-14 17:30:56 +0000

    Apply Rector changes (CI)

--- tests/Provider/QueryBuilderProvider.php
+++ tests/Provider/QueryBuilderProvider.php
@@ -586,7 +586,7 @@ final class QueryBuilderProvider extends \Yiisoft\Db\Tests\Provider\QueryBuilder
 
     public static function multiOperandFunctionBuilder(): array
     {
-        $data = parent::multiOperandFunctionBuilder();
+        $data = null;
 
         $stringParam = new Param('{3,4,5}', DataType::STRING);
 
