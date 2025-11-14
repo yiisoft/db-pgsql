4d7f787ab6f45ca49d85637b294404e8b16e7b8b
Sergei Predvoditelev <sergei@predvoditelev.ru>
2025-11-14 17:50:32 +0300

    replace `?float` to `int|float|null`

--- src/Expression/NumRangeValue.php
+++ src/Expression/NumRangeValue.php
@@ -9,8 +9,8 @@ use Yiisoft\Db\Expression\ExpressionInterface;
 final class NumRangeValue implements ExpressionInterface
 {
     public function __construct(
-        public readonly ?float $lower = null,
-        public readonly ?float $upper = null,
+        public readonly int|float|null $lower = null,
+        public readonly int|float|null $upper = null,
         public readonly bool $includeLower = true,
         public readonly bool $includeUpper = true,
     ) {}
