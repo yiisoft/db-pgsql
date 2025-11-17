0f5f5752d355533835153d8772039803bea63259
Sergei Predvoditelev <sergey.predvoditelev@gmail.com>
2025-11-17 11:10:47 +0300

    Update src/Column/Int4MultiRangeColumn.php
    Co-authored-by: Sergei Tigrov <rrr-r@ya.ru>

--- src/Column/Int4MultiRangeColumn.php
+++ src/Column/Int4MultiRangeColumn.php
@@ -8,6 +8,8 @@ use Yiisoft\Db\Schema\Column\ColumnInterface;
 
 final class Int4MultiRangeColumn extends AbstractMultiRangeColumn
 {
+    protected const DEFAULT_TYPE = PgsqlColumnType::INT4MULTIRANGE;
+
     protected function getRangeColumn(): ColumnInterface
     {
         return new Int4RangeColumn();
