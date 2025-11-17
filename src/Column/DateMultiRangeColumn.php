f18ff8004f49ef9a0b8aec9a865ef1f31862cbfa
Sergei Predvoditelev <sergey.predvoditelev@gmail.com>
2025-11-17 11:10:34 +0300

    Update src/Column/DateMultiRangeColumn.php
    Co-authored-by: Sergei Tigrov <rrr-r@ya.ru>

--- src/Column/DateMultiRangeColumn.php
+++ src/Column/DateMultiRangeColumn.php
@@ -8,6 +8,8 @@ use Yiisoft\Db\Schema\Column\ColumnInterface;
 
 final class DateMultiRangeColumn extends AbstractMultiRangeColumn
 {
+    protected const DEFAULT_TYPE = PgsqlColumnType::DATEMULTIRANGE;
+
     protected function getRangeColumn(): ColumnInterface
     {
         return new DateRangeColumn();
