3f6cf28cb5d6020379a641b4f12ef279ea67fe8c
Sergei Predvoditelev <sergey.predvoditelev@gmail.com>
2025-11-17 11:10:19 +0300

    Update src/Column/Int8MultiRangeColumn.php
    Co-authored-by: Sergei Tigrov <rrr-r@ya.ru>

--- src/Column/Int8MultiRangeColumn.php
+++ src/Column/Int8MultiRangeColumn.php
@@ -8,6 +8,8 @@ use Yiisoft\Db\Schema\Column\ColumnInterface;
 
 final class Int8MultiRangeColumn extends AbstractMultiRangeColumn
 {
+    protected const DEFAULT_TYPE = PgsqlColumnType::INT8MULTIRANGE;
+
     protected function getRangeColumn(): ColumnInterface
     {
         return new Int8RangeColumn();
