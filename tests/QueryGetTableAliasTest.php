f8282f377d7a2d6c3b6bc8689c2462ea08ce76af
Sergei Predvoditelev <sergei@predvoditelev.ru>
2025-11-18 13:27:10 +0300

    step 6

---
+++ tests/QueryGetTableAliasTest.php
@@ -0,0 +1,13 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Yiisoft\Db\Pgsql\Tests;
+
+use Yiisoft\Db\Pgsql\Tests\Support\IntegrationTestTrait;
+use Yiisoft\Db\Tests\Common\CommonQueryGetTableAliasTest;
+
+final class QueryGetTableAliasTest extends CommonQueryGetTableAliasTest
+{
+    use IntegrationTestTrait;
+}
