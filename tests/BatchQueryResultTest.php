3b04bc65b626764edce66d4e59ab36b4347ac736
Sergei Predvoditelev <sergei@predvoditelev.ru>
2025-11-18 15:13:20 +0300

    step 14

--- tests/BatchQueryResultTest.php
+++ tests/BatchQueryResultTest.php
@@ -4,15 +4,13 @@ declare(strict_types=1);
 
 namespace Yiisoft\Db\Pgsql\Tests;
 
-use Yiisoft\Db\Pgsql\Tests\Support\TestTrait;
+use Yiisoft\Db\Pgsql\Tests\Support\IntegrationTestTrait;
 use Yiisoft\Db\Tests\Common\CommonBatchQueryResultTest;
 
 /**
  * @group pgsql
- *
- * @psalm-suppress PropertyNotSetInConstructor
  */
 final class BatchQueryResultTest extends CommonBatchQueryResultTest
 {
-    use TestTrait;
+    use IntegrationTestTrait;
 }
