582c6e1330cf235ef002bc3e2249470d40cae736
Tigrov <rrr-r@ya.ru>
2026-02-07 15:03:12 +0700

    Improve performance of `SqlParser::getNextPlaceholder()` method

--- src/SqlParser.php
+++ src/SqlParser.php
@@ -8,6 +8,9 @@ use Yiisoft\Db\Syntax\AbstractSqlParser;
 
 final class SqlParser extends AbstractSqlParser
 {
+    /** @var string Identifier symbols, equals to `[$\w]` in regular expressions */
+    private const IDENTIFIER_CHARS = '$' . self::WORD_CHARS;
+
     public function getNextPlaceholder(?int &$position = null): ?string
     {
         $result = null;
@@ -71,17 +74,7 @@ final class SqlParser extends AbstractSqlParser
      */
     private function skipIdentifier(): void
     {
-        $continue = true;
-
-        while ($continue && $this->position < $this->length) {
-            match ($this->sql[$this->position]) {
-                '$', '_', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9',
-                'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u',
-                'v', 'w', 'x', 'y', 'z',
-                'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U',
-                'V', 'W', 'X', 'Y', 'Z' => ++$this->position,
-                default => $continue = false,
-            };
-        }
+        $length = strspn($this->sql, self::IDENTIFIER_CHARS, $this->position);
+        $this->position += $length;
     }
 }
